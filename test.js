797e863011e1191868251fd3efed36cb59193d04
Pedram Emrouznejad <p3pedram@gmail.com>
2016-02-03 15:56:36 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 console.log('travis', process.env.TRAVIS, process.env.TRAVIS === true, process.env.TRAVIS === 'true')
+console.log('ci', process.env.CI, process.env.CI === true, process.env.CI === 'true')
